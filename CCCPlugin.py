368103928cccd090971cbe3564b8b7a7f25e4e7b
David Saxon <davidesaxon@gmail.com>
2014-06-24 03:51:01 +1200

    cleaned up plugin script

--- CCCPlugin.py
+++ CCCPlugin.py
@@ -5,11 +5,5 @@ class CppCreateClassCommand(sublime_plugin.WindowCommand):
 
     def run(self):
 
-        try:
-            call(["ls"])
-            call(["python", "/home/david/.config/sublime-text-2/Packages/" +
-                "Cpp-Create-Class/CppCreateClass.py"])
-            print "fuck"
-        except Exception as e:
-            print e
-        print "Hello World"
+        call(["python", "/home/david/.config/sublime-text-2/Packages/" +
+            "Cpp-Create-Class/CppCreateClass.py"])
